eef9e386c4b1ab5113f3821fa1638b43958b9651
NicolasMCompSci <115744859+NicolasMCompSci@users.noreply.github.com>
2022-10-31 19:30:56 -0400

    Commit #3 - subtract() fix

--- CSCN71020_A1/main.c
+++ CSCN71020_A1/main.c
@@ -3,6 +3,7 @@
 void printWelcomeMenu();
 void printOptions();
 void add();
+void subtract();
 
 
 void main() {
@@ -20,6 +21,8 @@ void main() {
 	{
 	case 1:
 		add();
+	case 2:
+		subtract();
 	}
 
 }
@@ -44,4 +47,15 @@ void add() {
 	scanf_s("%lf", &num2);
 	result = num1 + num2;
 	printf("%lf + %lf = %lf\n", num1, num2, result);
+}
+
+void subtract()
+{
+	double num1, num2, result;
+	printf("Enter the first value (number to be subtracted):");
+	scanf_s("%lf", &num1);
+	printf("Enter the second value (number to subtract by):");
+	scanf_s("%lf", &num2);
+	result = num1 - num2;
+	printf("%lf - %lf = %lf\n", num1, num2, result);
 }
